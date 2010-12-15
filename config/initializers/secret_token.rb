1b87afc6227de11eca7ec5461c588c6862b103eb
Grant Speelman <grantspeelman@gmail.com>
2010-12-15 12:54:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grantspeelman::Application.config.secret_token = '36f69879d8266be67f8a331e6cb56a36b53efd09ab90f2a8ca339d3d2849c23359c4f4ae3e3f7b6a0c53b3e4cc7e601e73cf7c20dc29c5256abe665d3e3b3b62'
